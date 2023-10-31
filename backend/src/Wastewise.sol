a53565aa077068ba7380f054db462249d61b884e
MarsIfeanyi <marschalice@gmail.com>
2023-10-31 23:50:05 +0100

    Update deposit function to mint receipt token upon successful deposit

--- backend/src/Wastewise.sol
+++ backend/src/Wastewise.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.7.0 <0.9.0;
+import {RwasteWise} from "./RwasteWise.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract WasteWise{
-    struct User{
+contract WasteWise {
+    RwasteWise rwasteWise;
+    struct User {
         uint Id;
         address userAddr;
         string name;
@@ -15,18 +18,31 @@ contract WasteWise{
         string referral;
         uint TokenQty;
     }
-    enum Gender{
-        Female, Male
+    enum Gender {
+        Female,
+        Male
     }
-    struct Recycled{
+    struct Recycled {
         uint timeRecycled;
         uint qtyRecycled;
     }
     mapping(address => Recycled) RecycledMap;
     mapping(address => User) UserMap;
-     uint public userId;
-     error UserAcctNotCreated();
-        function createUserAcct(string memory _name, string memory _country, Gender _gender, uint _phone,string memory _email) external {
+
+    uint public userId;
+
+    // Custom Errors
+    error UserAcctNotCreated();
+
+    constructor() {}
+
+    function createUserAcct(
+        string memory _name,
+        string memory _country,
+        Gender _gender,
+        uint _phone,
+        string memory _email
+    ) external {
         userId++;
         User storage user = UserMap[msg.sender];
         user.Id = userId;
@@ -38,14 +54,22 @@ contract WasteWise{
         user.email = _email;
         user.timeJoined = block.timestamp;
     }
-    function  depositPlastic(uint _qtyrecycled) external{
+
+    function depositPlastic(uint _qtyrecycled) external {
         User storage user = UserMap[msg.sender];
         Recycled storage recycled = RecycledMap[msg.sender];
-        if(user.userAddr != msg.sender){
+        if (user.userAddr != msg.sender) {
             revert UserAcctNotCreated();
         }
-    
+
         recycled.qtyRecycled = _qtyrecycled;
-        recycled.timeRecycled = block.timestamp;        
+        recycled.timeRecycled = block.timestamp;
+
+        // Updates user TokenQty
+        user.TokenQty = user.TokenQty + _qtyrecycled;
+        // create new contract instance
+        rwasteWise = new RwasteWise();
+        // mints receiptTokens of the same amount, `_qtyrecycled` to user upon successful recycling
+        rwasteWise.mintReceipt(msg.sender, _qtyrecycled);
     }
 }
