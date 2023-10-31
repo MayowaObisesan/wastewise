2ac8c46d50e48e511039682ca85fc251e389d12b
Isaac <teryima127@gmail.com>
2023-11-01 00:24:17 +0100

    Natspec commenting

--- backend/src/Wastewise.sol
+++ backend/src/Wastewise.sol
@@ -3,45 +3,48 @@ pragma solidity >=0.7.0 <0.9.0;
 import {RwasteWise} from "./RwasteWise.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/// @title WasteWise: A smart contract for managing user recycling information.
 contract WasteWise {
-    RwasteWise rwasteWise;
     struct User {
         uint Id;
         address userAddr;
         string name;
         string country;
         Gender gender;
+        Recycled recycled;
         uint phone_no;
         string email;
         uint timeJoined;
-        address referral;
-        uint tokenQty;
+        string referral;
+        uint TokenQty;
     }
+
     enum Gender {
-        Female,
-        Male
+        Female, Male
     }
+
     struct Recycled {
         uint timeRecycled;
         uint qtyRecycled;
     }
-    mapping(address => Recycled[]) RecycledMap;
+
+    /// @dev Mapping to track recycled data for each user.
+    mapping(address => Recycled) RecycledMap;
+
+    /// @dev Mapping to store user data.
     mapping(address => User) UserMap;
 
-    uint public userId;
+    uint public userId; // Variable to keep track of the total number of users.
 
-    // Custom Errors
-    error UserAcctNotCreated();
+    error UserAcctNotCreated(); // Custom error for when a user account is not created.
 
-    constructor() {}
-
-    function createUserAcct(
-        string memory _name,
-        string memory _country,
-        Gender _gender,
-        uint _phone,
-        string memory _email
-    ) external {
+    /// @dev Create a new user account.
+    /// @param _name The user's name.
+    /// @param _country The user's country.
+    /// @param _gender The user's gender (0 for Female, 1 for Male).
+    /// @param _phone The user's phone number.
+    /// @param _email The user's email address.
+    function createUserAcct(string memory _name, string memory _country, Gender _gender, uint _phone, string memory _email) external {
         userId++;
         User storage user = UserMap[msg.sender];
         user.Id = userId;
@@ -54,27 +57,24 @@ contract WasteWise {
         user.timeJoined = block.timestamp;
     }
 
+    /// @dev Record the quantity of plastic recycled by a user.
+    /// @param _qtyrecycled The quantity of plastic recycled.
     function depositPlastic(uint _qtyrecycled) external {
         User storage user = UserMap[msg.sender];
-        if(user.userAddr != msg.sender){
-            revert UserAcctNotCreated();
+        Recycled storage recycled = RecycledMap[msg.sender];
+        if (user.userAddr != msg.sender) {
+            revert UserAcctNotCreated(); // Revert if the user account does not exist.
         }
-        Recycled memory recycled;
-        recycled.qtyRecycled = _qtyrecycled;
-        recycled.timeRecycled = block.timestamp;  
-        RecycledMap[msg.sender].push(recycled);
 
-        // Updates user TokenQty
-        user.TokenQty = user.TokenQty + _qtyrecycled;
-        // create new contract instance
-        rwasteWise = new RwasteWise();
-        // mints receiptTokens of the same amount, `_qtyrecycled` to user upon successful recycling
-        rwasteWise.mintReceipt(msg.sender, _qtyrecycled);
+        recycled.qtyRecycled = _qtyrecycled;
+        recycled.timeRecycled = block.timestamp;
     }
 
+    /// @dev Edit user information.
+    /// @param _user The updated user information.
     function editUser(User calldata _user) public {
-        if(UserMap[_user.userAddr].userAddr != _user.userAddr){
-            revert UserAcctNotCreated();
+        if (UserMap[_user.userAddr].userAddr != _user.userAddr) {
+            revert UserAcctNotCreated(); // Revert if the user account does not exist.
         }
         User storage user = UserMap[_user.userAddr];
         user.name = _user.name;
@@ -82,8 +82,5 @@ contract WasteWise {
         user.email = _user.email;
         user.phone_no = _user.phone_no;
         user.gender = _user.gender;
-
     }
-
-    
 }
