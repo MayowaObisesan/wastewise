c4432262f4ddc54570a8eb42b0dac635f3b45577
Isaac <teryima127@gmail.com>
2023-11-01 09:56:53 +0100

    Natspec commenting

--- backend/src/MarketPlace.sol
+++ backend/src/MarketPlace.sol
@@ -4,39 +4,53 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/// @title MarketPlace: A smart contract for managing item listings in a marketplace.
 contract MarketPlace {
+    /// @dev Structure to represent information about an item listing.
     struct ItemInfo {
-        string name;
-        string description;
-        uint256 price;
-        uint256 deadline;
-        address lister;
-        bool isActive;
-        uint256 itemId;
+        string name; 
+        string description; 
+        uint256 price; 
+        uint256 deadline; 
+        address lister; 
+        bool isActive; 
+        uint256 itemId; 
     }
 
+    /// @dev Mapping to store item information by their unique listing ID.
     mapping(uint256 => ItemInfo) public itemInfoToId;
+
+    /// @dev The address of the administrator of the marketplace.
     address public admin;
+
+    /// @dev The ID to assign to the next listing.
     uint256 public listingId;
 
     /* ERRORS */
 
     error MinPriceTooLow();
-    error DeadlineTooSoon();
+    error DeadlineTooSoon(); 
     error MinDurationNotMet();
     error InvalidSignature();
     error ListingDoesNotExist();
     error ListingNotActive();
-    error PriceNotMet(int256 difference);
-    error ListingExpired();
-    error PriceMismatch(uint256 originalPrice);
+    error PriceNotMet(int256 difference); 
+    error ListingExpired(); 
+    error PriceMismatch(uint256 originalPrice); 
 
     /* EVENTS */
 
     constructor() {
-        admin = msg.sender;
+        admin = msg.sender; // Set the contract deployer as the admin.
     }
 
+    /// @dev Create a new item listing in the marketplace.
+    /// @param _name The name of the item.
+    /// @param _description Description of the item.
+    /// @param _price Price of the item.
+    /// @param _deadline Deadline for the item listing.
+    /// @param _isActive Flag to indicate if the listing is active.
+    /// @return _listingId The unique identifier assigned to the new listing.
     function createListing(
         string calldata _name,
         string calldata _description,
@@ -44,33 +58,31 @@ contract MarketPlace {
         uint256 _deadline,
         bool _isActive
     ) public returns (uint256 _listingId) {
-        if (_price < 0.01 ether) revert MinPriceTooLow();
-        // check if deadline is lessthan currentTime
-        if (block.timestamp + _deadline <= block.timestamp)
-            revert DeadlineTooSoon();
-        // check if deadline is lessthan 60 minutes
-        if (_deadline - block.timestamp < 60 minutes)
-            revert MinDurationNotMet();
-
-        // append to Storage
+        if (_price < 0.01 ether) revert MinPriceTooLow(); 
+        if (block.timestamp + _deadline <= block.timestamp) revert DeadlineTooSoon(); 
+        if (_deadline - block.timestamp < 60 minutes) revert MinDurationNotMet(); 
+        // Append item information to storage.
         ItemInfo storage newItemInfo = itemInfoToId[listingId];
-
         newItemInfo.name = _name;
         newItemInfo.description = _description;
         newItemInfo.price = _price;
         newItemInfo.deadline = _deadline;
         newItemInfo.lister = msg.sender;
         newItemInfo.isActive = _isActive;
-
         _listingId = listingId;
-
         listingId++;
-
         return listingId;
     }
 
+    /// @dev Redeem receipt tokens for a transaction.
     function redeemReciptToken() public payable {}
 
+    /// @dev Update the information of an existing item listing.
+    /// @param _name The new name of the item.
+    /// @param _description New description of the item.
+    /// @param _listingId The unique identifier of the item listing to update.
+    /// @param _newPrice New price for the item.
+    /// @param _isActive Flag to indicate if the listing is active.
     function updateListing(
         string calldata _name,
         string calldata _description,
@@ -79,9 +91,11 @@ contract MarketPlace {
         bool _isActive
     ) public {}
 
-    function getItemInfo(
-        uint256 _listingId
-    ) public view returns (ItemInfo memory) {
+    /// @dev Get information about an item listing by its unique identifier.
+    /// @param _listingId The unique identifier of the item listing.
+    /// @return ItemInfo The information about the item listing.
+    function getItemInfo(uint256 _listingId) public view returns (ItemInfo memory) {
         return itemInfoToId[_listingId];
     }
 }
+
