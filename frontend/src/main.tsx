83447ec8b8e47735a3a50892b4206a0e56aade51
MarsIfeanyi <marschalice@gmail.com>
2023-11-03 21:45:54 +0100

    Update main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,11 @@ import { WagmiConfig } from "wagmi";
 
 import { App } from "./App";
 import { config } from "./wagmi";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiConfig config={config}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </WagmiConfig>
   </React.StrictMode>
 );
