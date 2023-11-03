4783c74cd560b06302b5be8ff67bf9d5bfdd4f18
Joshaw-k <awinrin4bless@yahoo.com>
2023-11-03 16:32:23 +0100

    add dashboard & landing page

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Recycle from "../components/Recycle";
+import Reward from "../components/Reward";
+import { Connect } from "../components/Connect";
+import { Account } from "../components/Account";
+import { ERC20 } from "../components/ERC20";
+import { NetworkSwitcher } from "../components/NetworkSwitcher";
+import { useAccount } from "wagmi";
+
+type Props = {};
+
+const Landing = (props: Props) => {
+  const { isConnected } = useAccount();
+  return (
+    <>
+      <Navbar />
+      <Recycle />
+      <Reward />
+      <h1>wagmi + ERC20 + Vite</h1>
+
+      <Connect />
+
+      {isConnected && (
+        <>
+          <Account />
+          <ERC20 />
+          <NetworkSwitcher />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Landing;
