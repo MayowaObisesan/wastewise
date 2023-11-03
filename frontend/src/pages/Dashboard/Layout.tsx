b8516756cd59a232eee176dd88c66351671aae5c
MarsIfeanyi <marschalice@gmail.com>
2023-11-03 21:46:57 +0100

    Render Siderbar and Header component

--- frontend/src/pages/Dashboard/Layout.tsx
+++ frontend/src/pages/Dashboard/Layout.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Sidebar, Header } from "../../components/dashboard";
 type Props = {};
 
 export const Layout = (props: Props) => {
   return (
-    <div>
-      <div>Sidebar</div>
+    <div className=" flex flex-row">
+      <div className="">
+        <Sidebar />
+      </div>
       <div>
-        <div>Header</div>
+        <div>
+          <Header />
+        </div>
         <div>
           <Outlet />
         </div>
