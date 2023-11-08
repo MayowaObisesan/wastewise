2defb696746c6265ae699812de081749ee40d7ad
Joshaw-k <awinrin4bless@yahoo.com>
2023-11-08 18:53:21 +0100

    josh fix

--- frontend/src/pages/Dashboard/Marketplace.tsx
+++ frontend/src/pages/Dashboard/Marketplace.tsx
@@ -4,7 +4,11 @@ import image2 from "/evangeline-shaw-nwLTVwb7DbU-unsplash.jpg";
 import image3 from "/chuttersnap-aEnH4hJ_Mrs-unsplash.jpg";
 import image4 from "/samantha-gades-fIHozNWfcvs-unsplash.jpg";
 import image5 from "/delaney-van-JYVKaxAlp4A-unsplash.jpg";
-import { useContractRead } from "wagmi";
+import {
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
 import marketPlaceAbi from "../../../public/Marketplace.json";
 import { formatUnits } from "viem";
 import { Link } from "react-router-dom";
@@ -59,61 +63,71 @@ const Marketplace = (props: Props) => {
     },
   });
 
+  const { config } = usePrepareContractWrite({
+    address: "0xecb504d39723b0be0e3a9aa33d646642d1051ee1",
+    abi: marketPlaceAbi,
+    functionName: "buyListing",
+    args: [],
+  });
+  const {
+    data: payData,
+    isLoading: loading1,
+    isSuccess,
+    write,
+  } = useContractWrite(config);
+
   useEffect(() => {
-    setTimeout(() => setLoading(!loading), 2000);
+    setTimeout(() => setLoading(!loading), 1000);
   }, []);
   return (
     <div className="my-8">
       <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
-        {loading ? (
-          <Link to={`event/${index + 1}`} key={index}>
-            <div className="card w-80 sm:w-[28rem] md:w-80 bg-base-100 shadow-xl">
-              <figure>
-                <img src={item.image} alt="Shoes" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">
-                  {item.name}
-                  <div className="badge badge-secondary">NEW</div>
-                </h2>
-                <p>{item.description}</p>
-                <p>Ends: {formatDate(Number(item.deadline))}</p>
-                <div className="card-actions justify-between items-center mt-3">
-                  <Button name="Pay Now" />
-                  <h3 className="font-bold text-lg">
-                    {formatUnits(item.price, 18)} <span>CHIX</span>
-                  </h3>
-                </div>
-              </div>
-            </div>
-          </Link>
-        ) : (
-          listings.map((item, index) => {
-            return (
-              <Link to={`event/${index + 1}`} key={index}>
-                <div className="card w-80 sm:w-[28rem] md:w-80 bg-base-100 shadow-xl">
-                  <figure>
-                    <img src={item.image} alt="Shoes" />
-                  </figure>
+        {loading
+          ? [1, 2, 3, 4].map((item, index) => {
+              return (
+                <div
+                  className="card w-80 sm:w-[28rem] md:w-80 bg-base-100 shadow-xl animate-pulse"
+                  key={index}
+                >
+                  <div className="h-32 bg-gray-200 rounded-md dark:bg-gray-700"></div>
                   <div className="card-body">
-                    <h2 className="card-title">
-                      {item.name}
-                      <div className="badge badge-secondary">NEW</div>
-                    </h2>
-                    <p>{item.description}</p>
-                    <p>Ends: {formatDate(Number(item.deadline))}</p>
-                    <div className="card-actions justify-between items-center mt-3">
-                      <Button name="Pay Now" />
-                      <h3 className="font-bold text-lg">
-                        {formatUnits(item.price, 18)} <span>CHIX</span>
-                      </h3>
+                    <h2 className="card-title h-4 bg-gray-200 rounded-md dark:bg-gray-700"></h2>
+                    <p className="h-4 bg-gray-200 rounded-md dark:bg-gray-700"></p>
+                    <p className="h-4 bg-gray-200 rounded-md dark:bg-gray-700"></p>
+                    <div className="flex justify-between items-center mt-3">
+                      <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-10"></div>
+                      <h3 className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-10"></h3>
                     </div>
                   </div>
                 </div>
-              </Link>
-            );
-          })
-        )}
+              );
+            })
+          : listings.map((item, index) => {
+              return (
+                <Link to={`event/${index + 1}`} key={index}>
+                  <div className="card w-80 sm:w-[28rem] md:w-80 bg-base-100 shadow-xl">
+                    <figure>
+                      <img src={item.image} alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title">
+                        {item.name}
+                        <div className="badge badge-secondary">NEW</div>
+                      </h2>
+                      <p>{item.description}</p>
+                      <p>Ends: {formatDate(Number(item.deadline))}</p>
+                      <div className="card-actions justify-between items-center mt-3">
+                        {/* <Button name="Pay Now" /> */}
+                        <button className="btn btn-primary">Pay Now</button>
+                        <h3 className="font-bold text-lg">
+                          {formatUnits(item.price, 18)} <span>CHIX</span>
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
       </div>
     </div>
   );
