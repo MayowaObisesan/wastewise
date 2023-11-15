2c49a26ab73739326b25a3f1a57dc30239ccaeb5
MayowaObisesan <mayowaobi74@gmail.com>
2023-11-15 10:36:36 +0100

    Frontend changes or updates

--- frontend/src/components/CardOne.tsx
+++ frontend/src/components/CardOne.tsx
@@ -1,4 +1,22 @@
+import { useAccount, useContractRead } from "wagmi";
+import { useWasteWiseContext } from "../context";
+import {
+  TokenABI,
+  WASTEWISE_ADDRESS,
+  WASTEWISE_TOKEN_ADDRESS,
+  WasteWiseABI,
+} from "../../constants";
+import { formatEther } from "viem";
+
 const CardOne = () => {
+  const { address } = useAccount();
+  const { currentUser } = useWasteWiseContext();
+  const { data } = useContractRead({
+    address: WASTEWISE_TOKEN_ADDRESS,
+    abi: TokenABI,
+    functionName: "totalSupply",
+  });
+
   return (
     <div className="border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-0 dark:shadow-md dark:bg-base-300 rounded-xl">
       <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-[#026937]">
@@ -24,7 +42,7 @@ const CardOne = () => {
       <div className="mt-4 flex items-end justify-between">
         <div>
           <h4 className="text-title-md font-bold text-black dark:text-white">
-            $3.456K
+            {formatEther(data)}
           </h4>
           <span className="text-sm font-medium">Total views</span>
         </div>
